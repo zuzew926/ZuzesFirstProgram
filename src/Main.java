dff7270c90f84c165db5308de28bedba9681d55b
ZuzeWu <zuzew@nycstudents.net>
2026-09-24 15:10:23 -0400

    i'm commiting (U1T3)

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int APPLES_PURCHASED = 24;
-        double APPLE_UNIT_PRICE = 0.60;
+        Scanner scan = new Scanner(System.in);
 
-        int PINEAPPLE_PURCHASED = 2;
-        double PINEAPPLE_COST = 3.59;
+        System.out.println("Please enter your name (String only or my program breaks)");
+        String name = scan.nextLine();
+        System.out.println("Hello, "+name+"!");
 
-        int BAGS_OF_RICE = 3;
-        int BAG_OF_RICE_KILO_WEIGHT = 2;
-        double RICE_UNIT_PRICE_POUND = 1.39;
-        double KILOGRAM_POUND_RATIO = 2.205;
+        System.out.println("Enter an integer!");
+        int num1 = scan.nextInt();
 
-        double RICE_COST = ((BAGS_OF_RICE * BAG_OF_RICE_KILO_WEIGHT) * KILOGRAM_POUND_RATIO) * RICE_UNIT_PRICE_POUND;
-        double TOTAL_PRICE = (APPLES_PURCHASED * APPLE_UNIT_PRICE + PINEAPPLE_PURCHASED * PINEAPPLE_COST + RICE_COST);
+        System.out.println("Enter another integer!");
+        int num2 = scan.nextInt();
 
-        System.out.println("Customer Name: Stacey");
-        System.out.println("Items purchased: $" + (APPLES_PURCHASED + PINEAPPLE_PURCHASED + BAGS_OF_RICE));
-        System.out.println("Total price: $" + TOTAL_PRICE);
-        System.out.println("Average cost per item: $" + ((RICE_COST + (PINEAPPLE_COST * PINEAPPLE_PURCHASED + APPLES_PURCHASED * APPLE_UNIT_PRICE)) / 3));
-        System.out.println("Amount tendered: $" + 40);
-        System.out.println("Change: $" + (40 - TOTAL_PRICE));
+        System.out.println("Enter a third integer!");
+        int num3 = scan.nextInt();
 
-        System.out.println("Hey I have to make a new change");
+        System.out.println("The average of your three numbers is...");
+        double average = (num1 + num2 + num3) / 3.0;
+
+        System.out.println(average+"!");
     }
 }
